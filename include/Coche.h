7c181ae5091e08c31d28a3b7ae8f63f748f8f064
Enrique Vicent <eccnil@gmail.com>
2019-06-23 00:24:41 +0200

    giro de marcha atrás

--- include/Coche.h
+++ include/Coche.h
@@ -5,7 +5,7 @@
 
 class Coche {
     // maxima diferencia de potencia en las ruedas de cada lado
-    static const int maxDeltaGiro = 60;
+    static const int maxDeltaGiro = 80;
     int velocidad = 0;
     int giro = 0;
     void actualizaRuedas();
@@ -43,13 +43,11 @@ void Coche::actualizaRuedas(){
     d = velocidad + delta;
     i = velocidad - delta;
 
-    if (d>100) {i -= d-100; d = 100;}
-    if (i>100) {d -= i-100; i = 100;}
-    if (d< -100) {i += d - -100; d = -100;}
-    if (i< -100) {d += i - -100; i = -100;}
-    Serial.println("potencia a los motores");
-    Serial.println(d);
-    Serial.println(i);
+    if (d > 100) { i -= d-100; d = 100; }
+    if (i > 100) { d -= i-100; i = 100; }
+    if (d < -100) { i -= d - -100; d = -100; }
+    if (i < -100) { d -= i - -100; i = -100; }
+    Serial.print("potencia a los motores: " );Serial.print(d);Serial.print(" , ");Serial.println(i);
     ruedaDer->move(d);
     ruedaIzq->move(i);
 }
